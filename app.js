11ac3d942c4020c3ce7db6459c8f1cb440f2ccf6
Kelvin Chao <kelvinchao2011@gmail.com>
2022-03-27 16:52:27 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+/* README GENERATOR */
+
+/* NEEDED:
+- Accepts CLI
+- Info: Name, 
+        title, 
+        sections + description, 
+        table of contents,
+        installation,
+        usage,
+        License,
+        contributing,
+        tests, 
+        questions
+*/
+
+/* Requirements: 
+  1. Enter title: displays title
+  2. enter CLI info -> display on readme
+  3. Licences -> checkboxes w/ options -> badges
+  4. Github username -> link to github
+  5. Email address -> added to section of readme with questions to contact
+  6. click links on table of contents -> goes to respective section
+  7. Uses the inquirer package
+*/
+
